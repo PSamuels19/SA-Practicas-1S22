415fb31f529c134be6ce2eefa03986a483b5ae3d
Peter Samuels <p_samuels19@outlook.com>
2022-03-19 00:45:08 -0600

    Update data.tf

--- Practica_7/vm/data.tf
+++ Practica_7/vm/data.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project     = var.gcp_project_id
-  region      = var.gcp_zone
+  region      = var.gcp_region
 }
 
 data "google_compute_network" "vpc" {
@@ -10,10 +10,10 @@ data "google_compute_network" "vpc" {
 
 data "google_compute_subnetwork" "subnet-1" {
   name   = var.gcp_subnet_1  
-  region = var.gcp_zone
+  region = var.gcp_region
 }
 
 data "google_compute_zones" "available" {
-  region = var.gcp_zone  #"us-central1_c" 
+  region = var.gcp_region  #"us-central1" 
   status = "UP"
 }
