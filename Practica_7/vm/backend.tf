35365fd785a53fb286a3ef56a9e71792e1bcd67e
Peter Samuels <p_samuels19@outlook.com>
2022-03-19 00:32:46 -0600

    Update backend.tf

--- Practica_7/vm/backend.tf
+++ Practica_7/vm/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket          = "tfstate-bucket-4291-terraform-default"
+    bucket          = "sa-tfstate-bucket"
     prefix          = "vm/terraform.tfstate"
 
   }
