fe8b8cc8b556c4ba765ef08beff7283b3e644ee2
MacoChave <201020831@ingenieria.usac.edu.gt>
2022-03-18 20:42:45 -0600

    Agregado terraform y nodejs a gitignore

--- Practica_7/main.tf
+++ Practica_7/main.tf
@@ -1,35 +1,32 @@
 terraform {
-	required_providers {
-		google = {
-			source = "hasicorp/google"
-			version = ">= 4.0.0, < 5.0"
-		}
-	}
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
 }
 
 provider "google" {
-	credentials = file("/home/terraform-user.json")
-	project = "instance-terraform"
-	region = "us-west4"
-	zone = "us-west4-b"
+  credentials = file("./terraform-user.json")
+  project     = "My First Project"
+  region      = "us-east1"
+  zone        = "us-east1-c"
 }
 
 resource "google_compute_firewall" "firewall" {
-  name        = "f-externalssh"
-  network     = "default"
-  description = "Creates firewall rule targeting tagged instances"
-
+  name    = "f-externalssh"
+  network = "default"
   allow {
-    protocol  = "tcp"
-    ports     = ["80", "8080", "22"]
+    protocol = "tcp"
+    ports    = ["8080", "80", "22"]
   }
-
-  source_tags = ["0.0.0.0/0"]
-  target_tags = ["externalssh"]
+  source_ranges = ["0.0.0.0/0"]
+  source_tags   = ["externalssh"]
 }
 
-resource "google_compute_address" "static_address" {
-  name = "dir"
+resource "google_compute_address" "static_add" {
+  name       = "dir"
   depends_on = [google_compute_firewall.firewall]
 }
 
@@ -37,21 +34,21 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
-data "google_compute_instance" "vm_instance" {
-  name = "terraform_instance"
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
   machine_type = "e2-medium"
 
   boot_disk {
-  	initialize_params {
-  		image = "debian-10-buster-v20220310"
-  	}
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20220213"
+    }
   }
 
   network_interface {
-  	name = "google_compute_network.vpc_network.name"
+    network = google_compute_network.vpc_network.name
 
-  	access_config {
-  		nat_ip = google_compute_address.static_address.address
-  	}
+    access_config {
+      nat_ip = google_compute_address.static_add.address
+    }
   }
 }
